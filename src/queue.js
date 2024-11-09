f576e1b520bdbc3739989115cbe301fb134d854c
Lorenzo-StJohn <britishaccent@bk.ru>
2024-11-09 16:00:33 +0400

    implement enqueue method for Queue class

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -14,23 +14,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    let pointer = this;
+    if (pointer.value === undefined) {
+      this.value = value;
+      this.next = null;
+    } else {
+      while (pointer.next !== null) pointer = pointer.next;
+      pointer.next = {
+        value,
+        next: null,
+      };
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
